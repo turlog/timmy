a9ed3cde9786e8ed482f27ad44576da1062df186
Łukasz Zosiak <lukasz.zosiak@sabre.com>
2023-06-07 10:30:35 +0200

    simple app with ability to perform calendar selections

--- app/App.js
+++ app/App.js
@@ -1,24 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import { StyleSheet, StatusBar, Text, View, FlatList, Vibration } from 'react-native';
+
+import { Calendar } from 'react-native-calendars';
+
 
 export default function App() {
+
+  const SELECTIONS = [
+    undefined, 'YES', 'NO'
+  ]
+
+  const SELECTION_COLORS = [
+    null, 'green', 'red'
+  ]
+
+  const [selection, setSelection] = useState({});
+
+  const toggleSelection = (day) => {
+    Vibration.vibrate(100);
+    setSelection((current) => {return {...current,
+      [day.dateString]: SELECTIONS[(SELECTIONS.indexOf(selection[day.dateString])+1)%SELECTIONS.length]
+    }});
+  }
+
+  let dates = [...Object.keys(selection)]
+
+  dates.sort().reverse()
+
+  let markers = {}
+
+  dates.forEach(date => {
+    if (selection[date]) {
+      markers[date] = {color: SELECTION_COLORS[SELECTIONS.indexOf(selection[date])], textColor: 'white'}
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>TIMMY</Text>
-      <Text>A collaborative time management tool.</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>TIMMY</Text>
+      </View>
+      <View style={styles.calendar}>
+        <Calendar
+          onDayPress={toggleSelection}
+          markingType={'period'}
+          markedDates={markers}
+        />
+      </View>
+      <View style={styles.details}>
+        <FlatList
+          data={dates}
+          renderItem={item => (
+            (selection[item.item]) ? (
+                <View style={{flexDirection: 'row'}}>
+                <Text style={{flex: 2}}>{item.item}</Text>
+                <Text style={{flex: 2}}>{selection[item.item]}</Text>
+              </View>
+            ) : ''
+          )}
+          scrollsToTop={false}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    fontWeight: 'bold',
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    marginTop: StatusBar.currentHeight,
   },
+  header: {
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    borderRadius: 10,
+    padding: 5,
+  },
+  calendar: {
+    padding: 5,
+    borderColor: '#eee',
+    borderBottomWidth: 1,
+  },
+  details: {
+    flex: 5,
+    padding: 5,
+  }
 });
